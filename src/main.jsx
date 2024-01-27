d128247d20ff1b7132a0a7b8533b18bf393eecfe
rokartur <rokartur04@icloud.com>
2024-01-27 02:43:20 +0100

    feat: add custom cursor

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
 import './styles/globals.scss'
-
+import 'custom-cursor-react/dist/index.css'
+import CustomCursor from 'custom-cursor-react'
 import { render } from 'preact'
 import { HelmetProvider } from 'react-helmet-async'
 import Router from 'preact-router'
 import AsyncRoute from 'preact-async-route'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
 import Header from './components/header/header.jsx'
 import Home from './pages/home.jsx'
 import AboutMe from './pages/aboutme.jsx'
 import Portfolio from './pages/portfolio.jsx'
 import NotFound from './pages/404.jsx'
 import Footer from './components/footer/footer.jsx'
-
 import scrollToTop from './utils/scrollToTop.jsx'
-import AnimatedCursor from 'react-animated-cursor'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -24,21 +22,22 @@ export const App = () => {
 
 	return (
 		<>
-			<AnimatedCursor
-				color="254, 72, 28"
-				outerAlpha={0}
-				innerSize={0}
-				outerSize={32}
-				innerScale={.8}
-				outerScale={1.5}
-				outerStyle={{
-					border: '1px solid rgba(255, 255, 255, .3)',
-					mixBlendMode: 'difference',
+			<CustomCursor
+				targets={['.link', 'a', 'button']}
+				customClass='custom-cursor'
+				dimensions={84}
+				fill='#FFFFFF00'
+				smoothness={{
+					movement: 0.07,
+					scale: 0.1,
+					opacity: 1,
 				}}
-				trailingSpeed={7}
-				showSystemCursor={true}
+				strokeColor={'#FFF'}
+				strokeWidth={1}
+				opacity={.3}
+				targetOpacity={.3}
+				targetScale={1.4}
 			/>
-
 			<Header/>
 			<HelmetProvider>
 				<Router>
